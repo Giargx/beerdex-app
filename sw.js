e0c5a5701bfc0db400d5c6050686da57623378ec
Giargx <n4c6g7kndz@privaterelay.appleid.com>
2026-06-14 18:03:11 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'beerdex-v1.1';
+const CACHE_NAME = 'beerdex-v1.2'; // Cambiando questo numero forzerai un aggiornamento sui telefoni
 
-// Quando viene rilevato un nuovo file sw.js, forziamo l'installazione saltando l'attesa
+// Quando l'app vede un nuovo sw.js, forza l'installazione
 self.addEventListener('install', event => {
     self.skipWaiting();
 });
 
-// Quando il nuovo worker si attiva, puliamo le vecchie cache e prendiamo il controllo
+// Pulisce le vecchie versioni salvate nel telefono e attiva la nuova
 self.addEventListener('activate', event => {
     event.waitUntil(
         caches.keys().then(cacheNames => {
@@ -21,7 +21,7 @@ self.addEventListener('activate', event => {
     event.waitUntil(clients.claim());
 });
 
-// Intercettiamo le richieste di rete (per la modalità offline)
+// Intercetta il traffico dati (utile per l'offline)
 self.addEventListener('fetch', event => {
     event.respondWith(
         fetch(event.request).catch(() => {
